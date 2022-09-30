1953a623855efd6cb0cc89b70cdf865806796836
Esteban Cotton-Caballero <estebancotcab98@gmail.com>
2022-09-29 20:31:30 -0400

    added all pokemon to map

--- src/Components/AllPokemon.jsx
+++ src/Components/AllPokemon.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import { Container, Grid } from "@mui/material";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import PokemonCard from "./PokemonCard";
 
 function AllPokemon() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+  const [pokeCount, setPokeCount] = useState(0);
 
   useEffect(() => {
     const getPokemon = async () => {
       const pokemonFromServer = await fetchPokemon();
-      setPokemon(pokemonFromServer);
+      setPokemons(pokemonFromServer.results);
+      console.log(pokemons);
     };
     getPokemon();
-  }, [pokemon]);
+    console.log(pokeCount, pokemons);
+  }, [pokeCount]);
 
   const fetchPokemon = async () => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/1/
-    `);
-
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?offset=${pokeCount}&limit=21`
+    );
     const data = await res.json();
+
     return data;
   };
+
+  const handlePrevious = () => {
+    console.log("previous page");
+  };
+
+  const handleNextPage = async (e) => {
+    e.preventDefault();
+    setPokeCount(pokeCount + 21);
+  };
+
   return (
     <>
       <Container
@@ -30,7 +44,27 @@ function AllPokemon() {
         }}
       >
         <Grid container spacing={3}>
-          <PokemonCard pokemon={pokemon} />
+          {pokemons.map(function (pokemon, idx) {
+            return <PokemonCard key={idx} pokemon={pokemon} />;
+          })}
+          <Grid
+            item
+            xs={12}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div className="buttons">
+              <button onClick={handlePrevious} className="previousButton">
+                previous
+              </button>
+              <button onClick={handleNextPage} className="nextButton">
+                next
+              </button>
+            </div>
+          </Grid>
         </Grid>
       </Container>
     </>
--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import CatchingPokemonTwoToneIcon from "@mui/icons-material/CatchingPokemonTwoTone";
+import { useState, useEffect } from "react";
 
 function PokemonCard({ pokemon }) {
+  const [pokeData, setPokeData] = useState([]);
+  const [pokeTypes, setPokeTypes] = useState([]);
+  const [cardColor, setCardColor] = useState();
+
+  useEffect(() => {
+    const getPokeData = async () => {
+      const pokeDataFromServer = await fetchPokeData();
+      setPokeData(pokeDataFromServer);
+      setPokeTypes(pokeDataFromServer.types);
+      setCardColor(pokeDataFromServer.types[0].type.name);
+    };
+
+    getPokeData();
+  }, []);
+
+  const fetchPokeData = async () => {
+    const res = await fetch(pokemon.url);
+
+    const data = await res.json();
+
+    return data;
+  };
   return (
     <>
       <Grid item xs={12} md={6} lg={4}>
-        <div className={`pokemonCard`}>
+        <div className={`pokemonCard ${cardColor} `}>
           <Grid
             container
             sx={{
@@ -23,7 +46,7 @@ function PokemonCard({ pokemon }) {
                     alignItems: "center",
                   }}
                 >
-                  <div className="pokemonName">{pokemon.name}</div>
+                  <div className="pokemonName">{pokeData.name}</div>
                 </Grid>
                 <Grid
                   item
@@ -33,7 +56,23 @@ function PokemonCard({ pokemon }) {
                     alignItems: "center",
                   }}
                 >
-                  <div className="pokemonType">hello</div>
+                  {pokeTypes?.map(function (object, idx) {
+                    return (
+                      <div className="pokemonType" key={idx}>
+                        {object.type.name}
+                      </div>
+                    );
+                  })}
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <div className="pokemonType"> {pokeData.id}</div>
                 </Grid>
               </Grid>
             </Grid>
@@ -51,7 +90,7 @@ function PokemonCard({ pokemon }) {
               <span
                 className="pokemonImage"
                 style={{
-                  backgroundImage: `url("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png")`,
+                  backgroundImage: `url("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png")`,
                 }}
               ></span>
             </Grid>
